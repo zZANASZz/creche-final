ba76af5bd733751774f575dd76562351f5b116d0
zZANASZz <ounsky100@gmail.com>
2026-06-08 23:09:00 +0200

    fix supabase fallback

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://emlqrrwmahbpabqfourn.supabase.co'
-const supabaseKey = 'sb_publishable_GzhiFmwnQjYuoHn5SNdt-A_yc8Epwa1'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://emlqrrwmahbpabqfourn.supabase.co'
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_GzhiFmwnQjYuoHn5SNdt-A_yc8Epwa1'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
